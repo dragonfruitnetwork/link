1e4386428da9e577f8f0684ea4c6577c020d02c6
Albie Spriddell <albion@dragonfruit.network>
2020-05-13 20:01:49 +0100

    add steam market asset prices

---
+++ DragonFruit.Link/Economy/Objects/SteamGameEconomyAssetPriceInfo.cs
@@ -0,0 +1,55 @@
+﻿// DragonFruit Link API Copyright 2020 (C) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under the GNU GPLv3 License. Refer to the license.md file at the root of the repo for more info
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DragonFruit.Link.Economy.Objects
+{
+    public class SteamGameEconomyAssetPriceInfo
+    {
+        [JsonProperty("prices")]
+        private JObject RawPrices
+        {
+            set
+            {
+                var list = new Dictionary<string, uint>(value.Count);
+
+                foreach (var item in value)
+                {
+                    if (item.Key == "Unknown")
+                        continue;
+
+                    list.Add(item.Key, (uint)item.Value);
+                }
+
+                Prices = list;
+            }
+        }
+
+        public IDictionary<string, uint> Prices { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("classid")]
+        public string ClassId { get; set; }
+
+        [JsonProperty("class")]
+        public IEnumerable<SteamGameEconomyClassInfo> ClassInfo { get; set; }
+    }
+
+    public class SteamGameEconomyClassInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ DragonFruit.Link/Economy/Requests/SteamGameEconomyAssetPriceRequest.cs
@@ -0,0 +1,31 @@
+﻿// DragonFruit Link API Copyright 2020 (C) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under the GNU GPLv3 License. Refer to the license.md file at the root of the repo for more info
+
+using DragonFruit.Common.Data.Parameters;
+
+namespace DragonFruit.Link.Economy.Requests
+{
+    public class SteamGameEconomyAssetPriceRequest : SteamApiRequest, IForSteamApp, IHasOptionalLanguage
+    {
+        public override string Interface => "ISteamEconomy";
+        public override string InterfaceMethod => "GetAssetPrices";
+
+        public override int MethodVersion => 1;
+
+        public override bool RequireApiKey => true;
+
+        public SteamGameEconomyAssetPriceRequest(uint appId)
+        {
+            AppId = appId;
+        }
+
+        [QueryParameter("appid")]
+        public uint AppId { get; set; }
+
+        [QueryParameter("currency")]
+        public string? Currency { get; set; }
+
+        [QueryParameter("language")]
+        public string? LanguageCode { get; set; }
+    }
+}
