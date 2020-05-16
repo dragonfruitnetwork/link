2d832ddd7b43616bea87051444a580acf8f3f8c2
Albie Spriddell <albion@dragonfruit.network>
2020-05-16 18:42:10 +0100

    add trade offers listing request

---
+++ DragonFruit.Link/Economy/Requests/SteamUserTradeOffersRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DragonFruit.Common.Data.Parameters;
+
+namespace DragonFruit.Link.Economy.Requests
+{
+    public class SteamUserTradeOffersRequest : SteamApiRequest, IHasOptionalLanguage
+    {
+        public override string Interface => "IEconService";
+        public override string InterfaceMethod => "GetTradeOffers";
+
+        public override int MethodVersion => 1;
+
+        public override bool RequireApiKey => true;
+
+        [QueryParameter("get_sent_offers")]
+        public bool? IncludeSentOffers { get; set; } = true;
+
+        [QueryParameter("get_received_offers")]
+        public bool? IncludeRecievedOffers { get; set; } = true;
+
+        [QueryParameter("get_descriptions")]
+        public bool? IncludeItemDescriptions { get; set; } = true;
+
+        [QueryParameter("language")]
+        public string? LanguageCode { get; set; }
+
+        [QueryParameter("active_only")]
+        public bool? ActiveOnly { get; set; }
+
+        [QueryParameter("historical_only")]
+        public bool? InactiveOnly { get; set; }
+
+        public DateTimeOffset? ActiveOnlyAfter { get; set; }
+
+        [QueryParameter("time_historical_cutoff")]
+        private double? ActiveOnlyAfterEpoch => ActiveOnlyAfter?.Subtract(DateTimeOffset.UnixEpoch).TotalSeconds;
+    }
+}
