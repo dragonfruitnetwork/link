95e8197d41fb96d8b0b853592436c1986bc1c77a
Albie <albion@dragonfruit.network>
2020-11-29 17:17:42 +0000

    apply suggestion

--- DragonFruit.Link.Tests/Game/SteamGlobalGameStatsTests.cs
+++ DragonFruit.Link.Tests/Game/SteamGlobalGameStatsTests.cs
@@ -14,7 +14,7 @@ namespace DragonFruit.Link.Tests.Game
         [TestMethod]
         public void GetSteamGlobalGameStatsTest()
         {
-            var metric = new List<string> { "global.map.emp_isle" };
+            var metric = new[] { "global.map.emp_isle" };
             var stat = Client.GetGlobalGameStats(17740, metric);
 
             Assert.IsTrue(stat[metric.Single()] > 42510619710);
