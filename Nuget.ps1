3e034bed2e6d9b9a1e22bdf07891a5aad0177030
Albie <albion@dragonfruit.network>
2021-04-22 17:20:43 +0100

    update workflows

--- Nuget.ps1
+++ Nuget.ps1
@@ -1,22 +1,22 @@
 param ([string] $ApiKey, [string]$Suffix = "")
 
-#versioning info
+# versioning info
 $VERSION = "$(Get-Date -UFormat "%Y.%m%d").$($env:GITHUB_RUN_NUMBER)$($Suffix)"
 $WORKINGDIR = Get-Location
 
-#Build files
+# nuget clear & restore
+nuget locals all -clear
+nuget restore
 
+# build files
 Write-Output "Building Steam Link Version $VERSION"
-dotnet restore
 dotnet build -c Release /p:PackageVersion=$VERSION
 
-#pack into nuget files with the suffix if we have one
-
+# pack into nuget files with the suffix if we have one
 Write-Output "Publishing Steam Link Version $VERSION"
 dotnet pack ".\DragonFruit.Link\DragonFruit.Link.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
 
-#recursively push all nuget files created
-
+# recursively push all nuget files created
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
     dotnet nuget push $_ --api-key $ApiKey --source https://api.nuget.org/v3/index.json --force-english-output
 }
