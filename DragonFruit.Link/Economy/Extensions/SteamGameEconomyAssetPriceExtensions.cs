a725ca0500658242ce6e73ede1c3f9678b90b2fb
Albie <albion@dragonfruit.network>
2021-12-01 16:51:22 +0000

    add async overloads

--- DragonFruit.Link/Economy/Extensions/SteamGameEconomyAssetPriceExtensions.cs
+++ DragonFruit.Link/Economy/Extensions/SteamGameEconomyAssetPriceExtensions.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using DragonFruit.Common.Data;
 using DragonFruit.Link.Economy.Objects;
 using DragonFruit.Link.Economy.Requests;
@@ -23,6 +24,19 @@ namespace DragonFruit.Link.Economy.Extensions
         {
             var request = new SteamGameEconomyAssetPriceRequest(appId);
             return client.Perform<SteamGameEconomyAssetPriceResponse>(request, token)?.PriceInfo.Assets;
+        }
+
+        /// <summary>
+        /// Get the community market prices for all items offered by a specific app
+        /// </summary>
+        /// <param name="client">The <see cref="SteamApiClient"/> to use</param>
+        /// <param name="appId">The App Id to get market info for</param>
+        /// <param name="token">The <see cref="CancellationToken"/> to pass when performing the request</param>
+        /// <returns>A list of all the items and their prices in various currencies</returns>
+        public static async Task<IEnumerable<SteamGameEconomyAssetPriceInfo>> GetMarketPricesForAppAsync<T>(this T client, uint appId, CancellationToken token = default) where T : ApiClient, ISteamApiClient
+        {
+            var request = new SteamGameEconomyAssetPriceRequest(appId);
+            return (await client.PerformAsync<SteamGameEconomyAssetPriceResponse>(request, token).ConfigureAwait(false))?.PriceInfo.Assets;
         }
     }
 }
--- DragonFruit.Link/Economy/Extensions/SteamUserTradeHoldDurationExtensions.cs
+++ DragonFruit.Link/Economy/Extensions/SteamUserTradeHoldDurationExtensions.cs
@@ -4,6 +4,7 @@
 using DragonFruit.Link.Economy.Requests;
 using DragonFruit.Link.Economy.Responses;
 using System.Threading;
+using System.Threading.Tasks;
 using DragonFruit.Common.Data;
 
 namespace DragonFruit.Link.Economy.Extensions
@@ -22,6 +23,20 @@ namespace DragonFruit.Link.Economy.Extensions
         {
             var request = new SteamUserTradeHoldDurationRequest(targetUser, targetToken);
             return client.Perform<SteamUserTradeHoldDurationResponse>(request, token)?.HoldDurations;
+        }
+
+        /// <summary>
+        /// Get the estimated trade hold between the API key owner and another user
+        /// </summary>
+        /// <param name="client">The <see cref="SteamApiClient"/> to use</param>
+        /// <param name="targetUser">The SteamID of the target</param>
+        /// <param name="targetToken">The `token` parameter of the user's Trade URL</param>
+        /// <param name="token">The <see cref="CancellationToken"/> to pass when performing the request</param>
+        /// <returns>The info of all parties' hold durations and a final one that will be used in the real trade</returns>
+        public static async Task<SteamUserTradeHoldDurationContainer> GetTradeHoldDurationsAsync<T>(this T client, ulong targetUser, string targetToken, CancellationToken token = default) where T : ApiClient, ISteamApiClient
+        {
+            var request = new SteamUserTradeHoldDurationRequest(targetUser, targetToken);
+            return (await client.PerformAsync<SteamUserTradeHoldDurationResponse>(request, token).ConfigureAwait(false))?.HoldDurations;
         }
     }
 }
