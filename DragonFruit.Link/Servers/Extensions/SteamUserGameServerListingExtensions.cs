785128f3dbdb7291949a41d32611cfe6541a3b3e
Albie <albion@dragonfruit.network>
2021-12-02 12:58:10 +0000

    add server management async methods

--- DragonFruit.Link/Servers/Extensions/SteamUserGameServerListingExtensions.cs
+++ DragonFruit.Link/Servers/Extensions/SteamUserGameServerListingExtensions.cs
@@ -5,6 +5,7 @@ using DragonFruit.Link.Servers.Objects;
 using DragonFruit.Link.Servers.Requests;
 using DragonFruit.Link.Servers.Responses;
 using System.Threading;
+using System.Threading.Tasks;
 using DragonFruit.Common.Data;
 
 namespace DragonFruit.Link.Servers.Extensions
@@ -21,6 +22,18 @@ namespace DragonFruit.Link.Servers.Extensions
         {
             var request = new SteamUserGameServerListingRequest();
             return client.Perform<SteamUserGameServerListingResponse>(request, token)?.ServerListing;
+        }
+
+        /// <summary>
+        /// Get the API key owner's steam game servers
+        /// </summary>
+        /// <param name="client">The <see cref="SteamApiClient"/> to use</param>
+        /// <param name="token">The <see cref="CancellationToken"/> to pass when performing the request</param>
+        /// <returns>The API key owner's account standing and any server they have registered, for all games</returns>
+        public static async Task<SteamUserGameServerListing> GetGameServersAsync<T>(this T client, CancellationToken token = default) where T : ApiClient, ISteamApiClient
+        {
+            var request = new SteamUserGameServerListingRequest();
+            return (await client.PerformAsync<SteamUserGameServerListingResponse>(request, token).ConfigureAwait(false))?.ServerListing;
         }
     }
 }
