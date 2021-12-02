e02ff8524cbca704e100e487b84ca2eacefe5aee
Albie <albion@dragonfruit.network>
2021-12-02 13:03:32 +0000

    add steam store async extensions

--- DragonFruit.Link/Store/Extensions/SteamStoreAppListingExtensions.cs
+++ DragonFruit.Link/Store/Extensions/SteamStoreAppListingExtensions.cs
@@ -7,6 +7,7 @@ using DragonFruit.Link.Store.Objects;
 using DragonFruit.Link.Store.Requests;
 using Newtonsoft.Json.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace DragonFruit.Link.Store.Extensions
 {
@@ -24,13 +25,33 @@ namespace DragonFruit.Link.Store.Extensions
             var request = new SteamStoreAppListingRequest(appId);
             var response = client.Perform<JObject>(request, token);
 
+            return ProcessRawResult(response, appId.ToString());
+        }
+
+        /// <summary>
+        /// Get the store listing info for an app/game
+        /// </summary>
+        /// <param name="client">The <see cref="SteamApiRequest"/> to use</param>
+        /// <param name="appId">The App Id to get the listing for</param>
+        /// <param name="token">The <see cref="CancellationToken"/> to pass when performing the request</param>
+        /// <returns>A <see cref="SteamStoreAppListing"/></returns>
+        public static async Task<SteamStoreAppListing> GetStoreListingAsync(this ApiClient client, uint appId, CancellationToken token = default)
+        {
+            var request = new SteamStoreAppListingRequest(appId);
+            var response = await client.PerformAsync<JObject>(request, token).ConfigureAwait(false);
+
+            return ProcessRawResult(response, appId.ToString());
+        }
+
+        private static SteamStoreAppListing ProcessRawResult(JObject response, string appId)
+        {
             if (response == null)
             {
                 // todo custom exception
                 throw new SteamRequestFailedException();
             }
 
-            return response[appId.ToString()]!["data"].ToObject<SteamStoreAppListing>();
+            return response[appId]!["data"]!.ToObject<SteamStoreAppListing>();
         }
     }
 }
