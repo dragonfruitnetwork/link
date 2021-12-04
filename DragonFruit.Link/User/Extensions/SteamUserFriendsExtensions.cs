76f6d09048f4240979862543c1aea6b86a8d8244
Albie <albion@dragonfruit.network>
2021-12-04 17:33:15 +0000

    add user async tasks

--- DragonFruit.Link/User/Extensions/SteamUserFriendsExtensions.cs
+++ DragonFruit.Link/User/Extensions/SteamUserFriendsExtensions.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using DragonFruit.Common.Data;
 using DragonFruit.Link.User.Objects;
 using DragonFruit.Link.User.Requests;
@@ -24,6 +25,20 @@ namespace DragonFruit.Link.User.Extensions
         {
             var request = new SteamUserFriendsRequest(steamId);
             return client.Perform<SteamUserFriendsResponse>(request, token)?.Container.Friends;
+        }
+
+        /// <summary>
+        /// Gets the user's friends, as a list
+        /// </summary>
+        /// <param name="client">The <see cref="SteamApiRequest"/> to use</param>
+        /// <param name="steamId">The user's SteamID64</param>
+        /// <param name="token">The <see cref="CancellationToken"/> to pass when performing the request</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="SteamUserFriend"/> objects</returns>
+        /// <remarks>This will cause an exception if the user's friend list is set to something other than "Public"</remarks>
+        public static async Task<IEnumerable<SteamUserFriend>> GetUserFriendsAsync<T>(this T client, ulong steamId, CancellationToken token = default) where T : ApiClient, ISteamApiClient
+        {
+            var request = new SteamUserFriendsRequest(steamId);
+            return (await client.PerformAsync<SteamUserFriendsResponse>(request, token).ConfigureAwait(false))?.Container.Friends;
         }
     }
 }
